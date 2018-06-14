175b1b5373d2347157d542c589b85e9ca8fe45c8
syh <4718220222@qq.com>
2018-06-14 15:26:58 +0800

    init

---
+++ src/order.ts
@@ -0,0 +1,5 @@
+export class Order {
+  id: number;
+  orderName: string;
+  orderSn: string;
+}
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import {HomePage, NavigationDetailsPage} from '../home/home';
+import {NavController} from "ionic-angular";
+import {Events} from "ionic-angular";
 
 @Component({
   templateUrl: 'tabs.html'
@@ -13,7 +15,17 @@ export class TabsPage {
   tab2Root = AboutPage;
   tab3Root = ContactPage;
 
-  constructor() {
+  constructor(public nav: NavController,
+              public events: Events) {
+    events.subscribe('hihi', item => {
+      this.openNavDetailsPage(item);
+    })
+
 
   }
+  openNavDetailsPage(item) {
+    this.nav.push(NavigationDetailsPage, { item: item });
+  }
+
+
 }
