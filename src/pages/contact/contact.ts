6174d0514c4896d408ce09eaa531604ea69bf75b
syh <4718220222@qq.com>
2018-06-15 10:30:50 +0800

    mock

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -6,7 +6,36 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'contact.html'
 })
 export class ContactPage {
-
+  items = [
+    {
+      name: '周文瀚',
+      portrait:'/assets/imgs/sea.jpg'
+    },
+    {
+      name: '王振琼',
+      portrait:'/assets/imgs/p1.jpeg'
+    },
+    {
+      name: '韩伟勇',
+      portrait:'/assets/imgs/p2.jpeg'
+    },
+    {
+      name: '张三',
+      portrait:'/assets/imgs/pe.png'
+    },
+    {
+      name: '李四',
+      portrait:'/assets/imgs/p4.jpeg'
+    },
+    {
+      name: '王五',
+      portrait:'/assets/imgs/p5.jpeg'
+    },
+    {
+      name: '赵六',
+      portrait:'/assets/imgs/p6.jpg'
+    },
+  ]
   constructor(public navCtrl: NavController) {
 
   }
--- src/pages/me/me.ts
+++ src/pages/me/me.ts
@@ -13,12 +13,40 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'me.html',
 })
 export class MePage {
-
+  me = {
+    portrait:'/assets/imgs/sea.jpg',
+    nickName:'崇坤客服',
+    userName:'15977778888'
+  }
+  items = [
+    {
+      'title': '我的订单',
+      'icon': 'buffer',
+      'description': 'A powerful Javascript framework for building single page apps. Angular is open source, and maintained by Google.',
+      'color': '#E63135'
+    },
+    {
+      'title': '账务统计',
+      'icon': 'usd',
+      'description': 'The latest version of cascading stylesheets - the styling language of the web!',
+      'color': '#0CA9EA'
+    },
+    {
+      'title': '我的店铺',
+      'icon': 'codepen',
+      'description': 'The latest version of cascading stylesheets - the styling language of the web!',
+      'color': '#0CA9EA'
+    }
+  ]
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MePage');
+  }
+
+  openNavDetailsPage(item) {
+
   }
 
 }
